f5e10f07daac76659e3f3b3320894914dea5ca7b
ye <15888520997@sina.cn>
2023-10-17 10:07:43 +0800

    update version

--- guide.go
+++ guide.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	r := route.InitRoute()
-	log.Println("INFO: Server version -> 2.2，listening and serving HTTP on "+global.Host+":"+global.Port)
+	log.Println("INFO: Server version -> 2.3，listening and serving HTTP on "+global.Host+":"+global.Port)
 	if err := r.Run(global.Host+":"+global.Port); err != nil {
 		log.Println("ERROR: error start fail", err)
 	}
