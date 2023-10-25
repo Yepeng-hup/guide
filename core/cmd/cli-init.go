3c36cc5451d8e2f25f149e62dd3a8a6a4733cb51
ye <15888520997@sina.cn>
2023-10-25 13:42:29 +0800

    add sys start check

---
+++ core/cmd/cli-init.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"guide/global"
+	"log"
+)
+
+func CliInit (){
+	saveDataDir := global.SaveDataDir
+	w := global.WhiteFile
+	lsStartWhitelist := global.IsStartWhitelist
+	host := global.Host
+	port := global.Port
+	interfaceName := global.InterfaceName
+	if saveDataDir == ""{
+		log.Fatalln("ERROR: GUIDE_FILEDATA_DIR is undefined")
+	}else if w == ""{
+		log.Fatalln("ERROR: GUIDE_WHITE_LIST is undefined")
+	}else if host == ""{
+		log.Fatalln("ERROR: GUIDE_HOST is undefined")
+	}else if port == ""{
+		log.Fatalln("ERROR: GUIDE_PORT is undefined")
+	}else if lsStartWhitelist == ""{
+		log.Fatalln("ERROR: GUIDE_START_WHITE_LIST is undefined")
+	}else  if interfaceName == ""{
+		log.Fatalln("ERROR: GUIDE_INTERFACE_NAME is undefined")
+	}
+	return
+}
--- global/global.go
+++ global/global.go
@@ -20,13 +20,14 @@ var (
 	SaveDataDir = os.Getenv("GUIDE_FILEDATA_DIR")
 	Host = os.Getenv("GUIDE_HOST")
 	Port = os.Getenv("GUIDE_PORT")
-	whiteFile = os.Getenv("GUIDE_WHITE_LIST")
+	WhiteFile = os.Getenv("GUIDE_WHITE_LIST")
 	IsStartWhitelist = os.Getenv("GUIDE_START_WHITE_LIST")
 	InterfaceName = os.Getenv("GUIDE_INTERFACE_NAME")
 )
 
 
 func IpList()[]string{
-	whiteIpList := strings.Split(whiteFile, ",")
+	whiteIpList := strings.Split(WhiteFile, ",")
 	return whiteIpList
 }
+
