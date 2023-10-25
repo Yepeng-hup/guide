b7779d12518e4fc4851587e82c5a18c6f94c4b26
ye <15888520997@sina.cn>
2023-10-25 13:41:40 +0800

    add sys

--- guide.go
+++ guide.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"guide/core"
+	"guide/core/cmd"
 	"guide/global"
 	"guide/route"
 	"log"
@@ -10,13 +11,14 @@ import (
 
 
 func main() {
+	cmd.CliInit()
 	r := route.InitRoute()
 	err := core.CreateCronTable()
 	if err != nil {
 		log.Fatalln(err.Error(),"guide database init fail.")
 	}
 	fmt.Printf("\033[32m%s\033[0m\n", global.Logo)
-	log.Println("INFO: Server version -> 2.5，listening and serving HTTP on "+global.Host+":"+global.Port)
+	log.Println("INFO: Server version -> 2.5, listening and serving HTTP on "+global.Host+":"+global.Port)
 	if err := r.Run(global.Host+":"+global.Port); err != nil {
 		log.Println("ERROR: error start fail", err)
 	}
