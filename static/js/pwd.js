9f8db13d07ee6e5c13c3cbd3096a145c7c216aa1
ye <15888520997@sina.cn>
2023-11-28 10:57:38 +0800

    v1

---
+++ static/js/pwd.js
@@ -0,0 +1,55 @@
+function deleteUP() {
+    let items=document.getElementsByClassName('cb');
+    let len=items.length;
+    for (var i=len-1; i>=0;i--) {
+        let is_checkd = items[i].checked;
+        if (is_checkd) {
+            let divItems = items[i].parentNode.parentNode;
+            let divlr = divItems.innerText;
+            $.post(
+                {
+                    "url": "/pwd/delete",
+                    "data": {
+                        "pwd": divlr,
+                    }
+                },
+
+            );
+            divItems.parentNode.removeChild(divItems);
+        }
+    }
+}
+
+
+function catdPwd() {
+    let items=document.getElementsByClassName('cb');
+    let len=items.length;
+    for (var i=len-1; i>=0;i--) {
+        let is_checkd = items[i].checked;
+        if (is_checkd) {
+            let divItems = items[i].parentNode.parentNode;
+            let divlr = divItems.innerText;
+            $.post(
+                {
+                    "url": "/pwd/cat",
+                    "data": {
+                        "pwd": divlr,
+                    },
+                    "success": function (data) {
+                        if (data["code"] === 200) {
+                            textpj(data["dPassword"]);
+                            return
+                        } else {
+                            window.location = location.pathname;
+                        }
+                    },
+                    "fail": function (error) {
+                        console.log(error);
+                    }
+
+                },
+
+            );
+        }
+    }
+}
