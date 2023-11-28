574836fb0c0792760cba74115e6cb4b3d5720acd
ye <15888520997@sina.cn>
2023-11-28 15:56:12 +0800

    update file

--- guide.go
+++ guide.go
@@ -18,7 +18,7 @@ func main() {
 		log.Fatalln(err,"guide database init fail.")
 	}
 	fmt.Printf("\033[32m%s\033[0m\n", global.Logo)
-	log.Println("INFO: Server version -> 2.5, listening and serving HTTP on "+global.Host+":"+global.Port)
+	log.Println("INFO: Server version -> 3.0, listening and serving HTTP on "+global.Host+":"+global.Port)
 	if err := r.Run(global.Host+":"+global.Port); err != nil {
 		log.Println("ERROR: error start fail", err)
 	}
