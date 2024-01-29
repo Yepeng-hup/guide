fc3597d8ff6998a348a8cb55d18bdeb0050bd198
ye <15888520997@sina.cn>
2024-01-29 15:50:57 +0800

    update file

--- guide.go
+++ guide.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"guide/core"
 	"guide/core/cmd"
 	"guide/global"
@@ -17,7 +16,6 @@ func main() {
 	if err != nil {
 		log.Fatalln(err,"guide database init fail.")
 	}
-	fmt.Printf("\033[32m%s\033[0m\n", global.Logo)
 	log.Println("INFO: Server version -> 3.0, listening and serving HTTP on "+global.Host+":"+global.Port)
 	if err := r.Run(global.Host+":"+global.Port); err != nil {
 		log.Println("ERROR: error start fail", err)
