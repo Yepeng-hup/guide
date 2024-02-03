f41b39d7fa6803244566942076465f6d9933c6a0
ye <15888520997@sina.cn>
2024-02-03 09:59:56 +0800

    update file

--- static/js/file.js
+++ static/js/file.js
@@ -162,3 +162,159 @@ function updateContent() {
         }
     }
 }
+
+
+function textpj(text) {
+    let htmlData = `
+        <pre style="color: white">${text}</pre>
+    `;
+
+    let list = document.getElementById('texts');
+    list.innerHTML = htmlData;
+}
+
+
+function deleteCheckbox() {
+    let items=document.getElementsByClassName('cb');
+    let len=items.length;
+    for (var i=len-1; i>=0;i--) {
+        let is_checkd = items[i].checked;
+        if (is_checkd) {
+            let divItems = items[i].parentNode.parentNode;
+            let divlr = divItems.innerText;
+            $.post(
+                {
+                    "url": "/file/delete",
+                    "data": {
+                        "FDname": divlr,
+                        "FDpath": location.pathname
+                    }
+                },
+
+            );
+            divItems.parentNode.removeChild(divItems);
+        }
+    }
+}
+
+
+function catFileCheckbox() {
+    let items=document.getElementsByClassName('cb');
+    let len=items.length;
+    var checkboxes = document.getElementsByClassName('cb');
+    for (var i=len-1; i>=0;i--) {
+        let is_checkd = items[i].checked;
+        if (is_checkd) {
+            let divItems = items[i].parentNode.parentNode;
+            let divlr = divItems.innerText;
+            $.get(
+                {
+                    "url": "/file/cat",
+                    "data": {
+                        "fileName": divlr,
+                        "filePath": location.pathname
+                    },
+                    "success": function (data) {
+                        if (data["code"] === 200) {
+                            textpj(data["fileText"]);
+                            return
+                        } else {
+                            alertMontage("不支持的文件格式.","alert-danger");
+                            for (let i = 0; i < checkboxes.length; i++) {
+                                checkboxes[i].checked = false;
+                            }
+                            setTimeout(function() {
+                                window.location = location.pathname;
+                            }, 1000);
+                        }
+                    },
+                    "fail": function (error) {
+                        console.log(error);
+                    }
+                },
+            )
+        }
+    }
+}
+
+function down() {
+    var checkboxes = document.getElementsByClassName('cb');
+    for (let i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = false;
+    }
+}
+
+
+function ysCheckbox() {
+    let items=document.getElementsByClassName('cb');
+    let len=items.length;
+    for (var i=len-1; i>=0;i--) {
+        let is_checkd = items[i].checked;
+        if (is_checkd) {
+            let divItems = items[i].parentNode.parentNode;
+            let divlr = divItems.innerText;
+            $.post(
+                {
+                    "url": "/file/ys",
+                    "data": {
+                        "fileName": divlr,
+                        "filePath": location.pathname
+                    },
+                    "success": function (data) {
+                        if (data["code"] === 200) {
+                            alertMontage(data["message"],"alert-success");
+                            setTimeout(function() {
+                                window.location = location.pathname;
+                            }, 1000);
+                        } else {
+                            alertMontage("文件或目录压缩失败.","alert-danger");
+                            setTimeout(function() {
+                                window.location = location.pathname;
+                            }, 1000);
+                        }
+                    },
+                    "fail": function (error) {
+                        console.log(error);
+                    }
+                },
+            )
+        }
+    }
+}
+
+function jyCheckbox() {
+    let items=document.getElementsByClassName('cb');
+    let len=items.length;
+    for (var i=len-1; i>=0;i--) {
+        let is_checkd = items[i].checked;
+        if (is_checkd) {
+            let divItems = items[i].parentNode.parentNode;
+            let divlr = divItems.innerText;
+            $.post(
+                {
+                    "url": "/file/jy",
+                    "data": {
+                        "fileName": divlr,
+                        "filePath": location.pathname
+                    },
+                    "success": function (data) {
+                        if (data["code"] === 200) {
+                            alertMontage(data["message"],"alert-success");
+                            setTimeout(function() {
+                                window.location = location.pathname;
+                            }, 1000);
+                        } else {
+                            alertMontage("解压失败.","alert-danger");
+                            setTimeout(function() {
+                                window.location = location.pathname;
+                            }, 1000);
+                        }
+                    },
+                    "fail": function (error) {
+                        console.log(error);
+                    }
+                },
+            )
+        }
+    }
+}
