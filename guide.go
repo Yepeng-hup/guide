c593c3a21a72e31db96f0646f40966832f303f75
ye <15888520997@sina.cn>
2024-04-26 16:10:30 +0800

    update file

--- guide.go
+++ guide.go
@@ -8,20 +8,16 @@ import (
 	"log"
 )
 
-
 func main() {
 	cmd.CliInit()
 	r := route.InitRoute()
 	err := core.CreateGuideAllTable()
 	go core.InitUser()
 	if err != nil {
-		log.Fatalln(err,"guide database init fail.")
+		log.Fatalln(err, "guide database init fail.")
 	}
-	log.Println("INFO: Server version -> 3.5, listening and serving HTTP on "+global.Host+":"+global.Port)
-	if err := r.Run(global.Host+":"+global.Port); err != nil {
+	log.Println("INFO: Server version -> 4.0, listening and serving HTTP on " + global.Host + ":" + global.Port)
+	if err := r.Run(global.Host + ":" + global.Port); err != nil {
 		log.Println("ERROR: error start fail", err)
 	}
 }
-
-
-
